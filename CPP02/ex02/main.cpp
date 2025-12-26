91da1798767d48321ecbe08817e74fefb34ffab8
Asmae Bouanine <asbouani@e2r7p12.1337.ma>
2025-12-26 10:56:51 +0100

    add CPP module 02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asbouani <asbouani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 21:20:48 by asbouani          #+#    #+#             */
-/*   Updated: 2025/12/24 18:20:59 by asbouani         ###   ########.fr       */
+/*   Updated: 2025/12/26 10:55:22 by asbouani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,7 @@ int main(void)
     std::cout << a << std::endl;
    
     std::cout << b << std::endl;
-    Fixed c(10);
-    Fixed d(2);
-    std::cout << c * d << std::endl;
-    std::cout << c / d << std::endl;
+   
     std::cout << Fixed::max(a, b) << std::endl;
   
     
